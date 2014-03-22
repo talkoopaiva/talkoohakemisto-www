58902405e42d423b35a8451e19b14fa2278bd339
Albert Nazander <alizbazar@alizweb.com>
2014-03-22 12:33:27 +0200

    WIP router.js

--- app/router.js
+++ app/router.js
@@ -1,28 +1,17 @@
-define(function(require, exports, module) {
+define([
+    'jquery', 'underscore', 'backbone', 'layoutmanager', 'app',
+    //'views/details', 'views/form', 'views/listitem', 'views/list',
+    'models/voluntary-work',
+    'collections/voluntary-works', 'collections/worktypes', 'collections/municipalities'
+  ], function(
+    $, _, Backbone, Layout, app,
+    //DetailsView, EditView, ListView, ListItemView,
+    VoluntaryWork,
+    VoluntaryWorks, WorkTypes, Municipalities
+  ) {
   "use strict";
 
-  var app = require("app");
-
-  // External dependencies.
-  var Backbone = require("backbone");
-
-  var VoluntaryWork = require("modules/voluntary-work");
-  var Types = require("modules/types");
-  var Municipalities = require("modules/municipalities");
-
-  // Defining the application router.
-  module.exports = Backbone.Router.extend({
-    initialize: function() {
-
-      this.voluntaryWorks = new VoluntaryWork.Collection();
-      this.types = new Types.Collection();
-      this.municipalities = new Municipalities.Collection();
-
-      this.views = {
-
-      };
-    },
-
+  var VoluntaryWorkRouter = Backbone.Router.extend({
     routes: {
       "": "list",
       "list": "list",
@@ -30,64 +19,96 @@ define(function(require, exports, module) {
       "view/:id": "form"
     },
 
-    list: function() {
+    initialize: function() {
 
-      // Empty existing itemlist
-      //$('ul.itemList').html('');
-      if (!this.views.voluntaryWorkList) {
-        this.views.voluntaryWorkList = new VoluntaryWork.Views.List({collection: this.voluntaryWorks});
-        //console.log('list route created');
-      } else {
-        this.views.voluntaryWorkList.show();
-        //console.log('list route displayed');
-      }
-    },
+      app.voluntaryWorks = new VoluntaryWorks();
+      app.types = new WorkTypes();
+      app.municipalities = new Municipalities();
 
-    form: function(id) {
-      // If "id" is not present, new item creation is triggered
+      this.views = {
 
-      console.log('form route triggered');
-      var collection = this.voluntaryWorks;
-
-      $('#voluntaryWorkDetails').empty();
-
-      var router = this;
-      var constructs = {
-        collection: router.voluntaryWorks,
-        linkedItems: {
-          types: router.types,
-          municipalities: router.municipalities
-        }
       };
 
-      var dependencies = [];
-
-      // If id -> then it's edit, so we have to fetch model
-      if (id) {
-        // TODO: first look in the collection
-        constructs.model = new VoluntaryWork.Model({id: id});
-        dependencies.push(constructs.model.fetch());
-      }
-
-      if (this.municipalities.length < 1) {
-        dependencies.push(this.municipalities.fetch());
-      }
-
-      if (this.types.length < 1) {
-        dependencies.push(this.types.fetch());
-      }
-
-      $.when.apply(this, dependencies).done(function() {
-        var itemView = new VoluntaryWork.Views.Form(constructs).render();
-
-        // TODO: DOM manipulation should be done in View instead of router
-        $('#voluntaryWorkDetails').html(itemView.$el);
+      // For testing
+      var router = this;
+      _.extend(window, {
+        app: app,
+        router: router,
+        models: {
+          VoluntaryWork: VoluntaryWork
+        },
+        collections: {
+          VoluntaryWorks: VoluntaryWorks,
+          Municipalities: Municipalities,
+          Types: WorkTypes
+        }
       });
+    },
 
-      $('#voluntaryWorkList').hide();
-      $('#voluntaryWorkDetails').show();
+    list: function() {
 
     }
 
+    // list: function() {
+
+    //   // Empty existing itemlist
+    //   //$('ul.itemList').html('');
+    //   if (!this.views.voluntaryWorkList) {
+    //     this.views.voluntaryWorkList = new VoluntaryWork.Views.List({collection: this.voluntaryWorks});
+    //     //console.log('list route created');
+    //   } else {
+    //     this.views.voluntaryWorkList.show();
+    //     //console.log('list route displayed');
+    //   }
+    // },
+
+    // form: function(id) {
+    //   // If "id" is not present, new item creation is triggered
+
+    //   console.log('form route triggered');
+    //   var collection = this.voluntaryWorks;
+
+    //   $('#voluntaryWorkDetails').empty();
+
+    //   var router = this;
+    //   var constructs = {
+    //     collection: router.voluntaryWorks,
+    //     linkedItems: {
+    //       types: router.types,
+    //       municipalities: router.municipalities
+    //     }
+    //   };
+
+    //   var dependencies = [];
+
+    //   // If id -> then it's edit, so we have to fetch model
+    //   if (id) {
+    //     // TODO: first look in the collection
+    //     constructs.model = new VoluntaryWork.Model({id: id});
+    //     dependencies.push(constructs.model.fetch());
+    //   }
+
+    //   if (this.municipalities.length < 1) {
+    //     dependencies.push(this.municipalities.fetch());
+    //   }
+
+    //   if (this.types.length < 1) {
+    //     dependencies.push(this.types.fetch());
+    //   }
+
+    //   $.when.apply(this, dependencies).done(function() {
+    //     var itemView = new VoluntaryWork.Views.Form(constructs).render();
+
+    //     // TODO: DOM manipulation should be done in View instead of router
+    //     $('#voluntaryWorkDetails').html(itemView.$el);
+    //   });
+
+    //   $('#voluntaryWorkList').hide();
+    //   $('#voluntaryWorkDetails').show();
+
+    // }
+
   });
+
+  return VoluntaryWorkRouter;
 });
