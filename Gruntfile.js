01a4d316b49fd32cf3c677398b8099a2221ef2c6
Albert Nazander <alizbazar@alizweb.com>
2014-03-24 05:07:12 +0200

    Remove unused method

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,15 +46,17 @@ module.exports = function(grunt) {
           preserveLicenseComments: false,
 
           // This is needed for the require-handlebars library
-          onBuildWrite : function(moduleName, path, content){
-              // replace handlebars with the runtime version
-              if (moduleName === 'Handlebars') {
-                  path = path.replace('handlebars.js','handlebars.runtime.js');
-                  content = grunt.file.read(path).toString();
-                  content = content.replace(/(define\()(function)/, '$1"handlebars", $2');
-              }
-              return content;
-          }
+          // (didn't seem to match anything, not sure if it's needed /Albert)
+          // onBuildWrite : function(moduleName, path, content){
+          //     // replace handlebars with the runtime version
+          //     if (moduleName === 'Handlebars') {
+          //       console.log('matched: ' + moduleName + ' : ' + path);
+          //         path = path.replace('handlebars.js','handlebars.runtime.js');
+          //         console.log('newpath: ' + path);
+          //         content = content.replace(/(define\()(function)/, '$1"handlebars", $2');
+          //     }
+          //     return content;
+          // }
         }
       }
     },
