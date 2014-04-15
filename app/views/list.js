1a220ffa2fc6bc8bfc07737d94e1c9120c45cf50
Albert Nazander <alizbazar@alizweb.com>
2014-04-16 01:02:33 +0300

    Fix duplicate entries

--- app/views/list.js
+++ app/views/list.js
@@ -2,23 +2,18 @@ define(['module', 'underscore', 'backbone', 'jquery', 'app', 'views/listitem', '
         function(module, _, Backbone, $, app, ItemView, template) {
   module.exports = Backbone.Layout.extend({
     template: template,
-    addView: function(model, render) {
+    addView: function(model) {
       // Insert a nested View into this View.
       var view = this.insertView("#talkoot", new ItemView({ model: model }));
-
-      // Only trigger render if it not inserted inside `beforeRender`.
-      if (render !== false) {
-        view.render();
-      }
     },
 
     beforeRender: function() {
       this.collection.each(function(model) {
-        this.addView(model, false);
+        this.addView(model);
       }, this);
     },
 
-    initialize: function() {
+    afterRender: function() {
       this.listenTo(this.collection, "add", this.addView);
     }
   });
